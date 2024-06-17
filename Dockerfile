e2680bee8474e26220f71d9f08ab39aa510be08e
FrancoDominguez <franco@wearecws.com>
2024-06-17 16:55:28 -0400

    changed the way express is imported in server.js

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /app
 COPY package.json package-lock.json ./
 
 RUN npm install --silent
-RUN npm install express
 
 COPY . .
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
